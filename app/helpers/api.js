01a358478a2c7dfb9f7d1ac3a4b1d3822ca59adf
Joel Birch <joeldbirch@gmail.com>
2016-05-06 18:00:48 +1000

    Make get5DayCityForecast work.

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -8,7 +8,7 @@ const helpers = {
     // get 5 day forecast for city via API
     // http://api.openweathermap.org/data/2.5/forecast/daily?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY&cnt=5
     // my API key is: c4e386d5aa9717b232a9d0488d492818
-    return {}
+    return axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&type=accurate&APPID=${apiKey}&cnt=5`)
   },
 
   getCurrentCityWeather (location) {
